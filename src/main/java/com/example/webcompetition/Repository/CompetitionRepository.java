322c50184db9a50c995c5c352ed02c282894bae5
sucnsc <584156664@qq.com>
2018-06-01 12:22:14 +0800

    模糊搜索

--- src/main/java/com/example/webcompetition/Repository/CompetitionRepository.java
+++ src/main/java/com/example/webcompetition/Repository/CompetitionRepository.java
@@ -1,7 +1,6 @@
 package com.example.webcompetition.Repository;
 
 import com.example.webcompetition.Entity.Competition;
-import org.hibernate.sql.Update;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
@@ -10,6 +9,6 @@ public interface CompetitionRepository extends CrudRepository<Competition,Long>
 {
     List<Competition> findAll();
 
-    @Query(value = "select * from competition  where ((case when ?1='' then 1 end ) or year=?1) and ((case when ?2='' then 1 end ) or grade=?2) and ((case when ?3='' then 1 end ) or name=?3) and ((case when ?4='' then 1 end ) or student1=?4 or student2=?4 or student3=?4) and ((case when ?5='' then 1 end ) or teacher1=?5 or teacher2=?5) ",nativeQuery = true)
+    @Query(value = "select * from competition  where ((case when ?1='' then 1 end ) or year like %?1%) and ((case when ?2='' then 1 end ) or grade like %?2%) and ((case when ?3='' then 1 end ) or name like %?3%) and ((case when ?4='' then 1 end ) or student1 like %?4% or student2 like %?4% or student3 like %?4%) and ((case when ?5='' then 1 end ) or teacher1 like %?5% or teacher2 like %?5%) ",nativeQuery = true)
     List<Competition> findtest(String year,String grade,String name,String student,String teacher);
 }
