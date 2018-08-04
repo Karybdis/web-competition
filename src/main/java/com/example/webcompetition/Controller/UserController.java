4cf90bf6c541e39edf7e8c1067f21282c39fe97a
sucnsc <584156664@qq.com>
2018-08-04 22:41:23 +0800

    极小修改

--- src/main/java/com/example/webcompetition/Controller/UserController.java
+++ src/main/java/com/example/webcompetition/Controller/UserController.java
@@ -10,7 +10,6 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
